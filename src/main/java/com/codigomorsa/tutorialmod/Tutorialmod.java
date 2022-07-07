fbc005303855d77419530238a71d0f29b5eb52d0
Martin Cornejo <mart256@gmail.com>
2022-07-07 23:52:11 +0200

    Structured the package for the code, added mods.toml, added missing default resourcePack. Mod is now properly loaded.

---
+++ src/main/java/com/codigomorsa/tutorialmod/Tutorialmod.java
@@ -0,0 +1,8 @@
+package com.codigomorsa.tutorialmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Tutorialmod.MODID)
+public class Tutorialmod {
+    public static final String MODID = "tutorialmod";
+}
