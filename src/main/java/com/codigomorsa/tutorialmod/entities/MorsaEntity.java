0fcabb04adf7b759941a2401aa8606b4d74cdd6c
Martin Cornejo <mart256@gmail.com>
2022-09-17 11:28:17 +0200

    Diglet is tamable and follows owner

--- src/main/java/com/codigomorsa/tutorialmod/entities/MorsaEntity.java
+++ src/main/java/com/codigomorsa/tutorialmod/entities/MorsaEntity.java
@@ -2,22 +2,25 @@ package com.codigomorsa.tutorialmod.entities;
 
 import com.codigomorsa.tutorialmod.init.EntityInit;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
-import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.common.ForgeMod;
 import org.jetbrains.annotations.Nullable;
 
 
-public class MorsaEntity extends Animal {
+public class MorsaEntity extends TamableAnimal {
 
-    public MorsaEntity(EntityType<? extends Animal> type, Level level) {
+    public MorsaEntity(EntityType<? extends TamableAnimal> type, Level level) {
         super(type, level);
     }
 
@@ -30,17 +33,29 @@ public class MorsaEntity extends Animal {
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new FloatGoal(this));
-        this.goalSelector.addGoal(1, new PanicGoal(this, 1.25D));
+        this.goalSelector.addGoal(1, new FollowOwnerGoal(this, 1.0D, 10.0F, 2.0F, false));
+        this.goalSelector.addGoal(2, new LookAtPlayerGoal(this, Player.class, 6.0F));
+//        this.goalSelector.addGoal(1, new PanicGoal(this, 1.25D));
         this.goalSelector.addGoal(3, new BreedGoal(this, 1.0D));
         this.goalSelector.addGoal(5, new FollowParentGoal(this, 1.1D));
         this.goalSelector.addGoal(6, new WaterAvoidingRandomStrollGoal(this, 1.0D));
-        this.goalSelector.addGoal(7, new LookAtPlayerGoal(this, Player.class, 6.0F));
         this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
+    }
+
+    @Override
+    public boolean hurt(DamageSource p_27567_, float p_27568_) {
+        return false;
     }
 
     public static AttributeSupplier.Builder getMorsaEntityAttrs() {
         return Mob.createMobAttributes()
                 .add(ForgeMod.ENTITY_GRAVITY.get(), 1.5f)
-                .add(Attributes.MOVEMENT_SPEED, 0.7246D);
+                .add(Attributes.MOVEMENT_SPEED, 0.25D);
+    }
+
+    @Override
+    public InteractionResult mobInteract(Player player, InteractionHand p_27585_) {
+        this.tame(player);
+        return InteractionResult.SUCCESS;
     }
 }
